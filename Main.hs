9a5aee1a8e2c5073e1f57816aa9d001415864779
Travis Dixon <thetrav@Traviss-MacBook-Air.local>
2014-10-14 16:55:05 +1100

    make it as simple as I can

--- Main.hs
+++ Main.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
 import Network.Wai.Middleware.Static
-import Text.Blaze.Html.Renderer.Text
+import qualified Data.Text.Lazy as T
+
 import qualified Views.Index
-import qualified Web.Scotty as S
-blaze = S.html . renderHtml
 
 main = scotty 3000 $ do
   get "/" $ do
-    blaze Views.Index.render
+    html . T.pack  $ Views.Index.render "trav"
 
   middleware $ staticPolicy (noDots >-> addBase "public")
-
--- Views/Index.hs
+++ Views/Index.hs
@@ -1,9 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
 
 module Views.Index where
-	import Text.Blaze.Html5
-	import Text.Blaze.Html5.Attributes
-	render = do
-		html $ do
-			body $ do
-				h1 "Hello world"
+	render :: String -> String
+	render name = "Hello " ++ name
